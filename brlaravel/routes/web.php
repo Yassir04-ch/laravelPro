9d2a0c0545ff5a2ee8db5c6cb7c523e99e0a6dd7
yassir cherqui <yassirch245@gmail.com>
2026-01-28 22:27:37 +0100

    ajoute

--- brlaravel/routes/web.php
+++ brlaravel/routes/web.php
@@ -9,7 +9,8 @@ Route::get('/', function () {
 });
 
 // Route::get('categories',CategoryController::class);
-
+Route::get('/post/index',[PostController::class , "index"]);
+Route::get('/post/create',[PostController::class , "create"]);
 Route::resource('posts', PostController::class);
  
 Route::resource('categories', CategoryController::class);
